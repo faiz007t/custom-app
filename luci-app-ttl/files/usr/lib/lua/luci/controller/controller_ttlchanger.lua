7807ef917217eb6745b00a6d6f720a46254bde0a
faiz007t <faiz007t@github.com>
2025-06-08 21:54:27 +0800

    custom

--- luci-app-ttl/files/usr/lib/lua/luci/controller/controller_ttlchanger.lua
+++
@@ -1,6 +0,0 @@
-module("luci.controller.controller_ttlchanger", package.seeall)
-
-function index()
-    entry({"admin", "modem", "ttlchanger"}, cbi("model_ttlchanger"), _("TTL Settings"), 100)
-end
-
